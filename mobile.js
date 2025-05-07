c725890611566e9aad25c63ea35cbd340df447f5
Msari <alk220002@spengergasse.at>
2025-05-07 20:59:21 +0100

    Grosses_Update

---
+++ mobile.js
@@ -0,0 +1,4 @@
+function enableMobileVersion() {
+    const stylesheet = document.getElementById('main-stylesheet');
+    stylesheet.setAttribute('href', 'mobile-styles.css'); // Lädt die mobile CSS-Datei
+}
